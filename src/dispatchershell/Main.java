433bfda0d5a836d52472de680701c5098b1bbb12
Seidy KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-12-07 23:05:56 +0300

    Update

--- src/dispatchershell/Main.java
+++ src/dispatchershell/Main.java
@@ -5,14 +5,7 @@ public class Main {
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static void main(String[] args) {
 		Console console = new Console();
-<<<<<<< HEAD
 		console.log("Test commit for operating system project");	
-=======
-		console.log("00:00:00 Process1 of priority 2 is running...");
-		console.log("00:00:00 Process1 of priority 2 is running...");
-		System.out.print(ANSIYELLOW + "Seidy is kral");
-		
->>>>>>> a013763630b2b3795a69e134f2069cd2b25bba00
 	}
 
 }
