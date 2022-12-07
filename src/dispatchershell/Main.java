05c4d9e6a99b51d0763b20c040708381fbab25f1
Bedru umer <bedru2019@gmail.com>
2022-12-07 20:28:20 +0300

    test commit

--- src/dispatchershell/Main.java
+++ src/dispatchershell/Main.java
@@ -3,7 +3,6 @@ package dispatchershell;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Console console = new Console();
 		console.log("Test commit for operating system project");
 		
