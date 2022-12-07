f88de69711b323732c67064246ca1ed4908ad83b
Bedru umer <bedru2019@gmail.com>
2022-12-07 20:04:17 +0300

    initial commit

---
+++ src/dispatchershell/Console.java
@@ -0,0 +1,8 @@
+package dispatchershell;
+
+public class Console {
+	public void log(String text)
+	{
+		System.out.println(text);
+	}
+}
---
+++ src/dispatchershell/Main.java
@@ -0,0 +1,12 @@
+package dispatchershell;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Console console = new Console();
+		console.log("Hello world");
+		
+	}
+
+}
