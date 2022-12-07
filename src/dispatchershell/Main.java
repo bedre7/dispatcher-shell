4a9a0372a2eb5857e5eea01b90a89984e674b71b
Seidy KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-12-07 22:57:43 +0300

    Update

--- src/dispatchershell/Main.java
+++ src/dispatchershell/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Console console = new Console();
-		console.log("Test commit for operating system project");
-		
+		console.log("Test commit for operating system project");	
 	}
 
 }
