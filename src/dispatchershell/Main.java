82cff918650ca17cd81bed01f4cf995d99f84739
Seidy KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-12-07 20:26:21 +0300

    update

--- src/dispatchershell/Main.java
+++ src/dispatchershell/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Console console = new Console();
-		console.log("Hello world");
+		console.log("Test commit for operating system project");
 		
 	}
 
